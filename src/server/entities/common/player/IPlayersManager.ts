871788c811a7064fdfc2cbc2055abf8a4ae622b8
xvetal <djratmir@gmail.com>
2024-09-23 04:47:03 +0300

    Added player getting by name.

--- src/server/entities/common/player/IPlayersManager.ts
+++ src/server/entities/common/player/IPlayersManager.ts
@@ -1,4 +1,7 @@
 import { IEntitiesManager } from "../entity/IEntitiesManager";
 import { IPlayer } from "./IPlayer";
 
-export interface IPlayersManager extends IEntitiesManager<IPlayer> {}
+export interface IPlayersManager extends IEntitiesManager<IPlayer> {
+  getByName(name: string): IPlayer;
+  findByName(name: string): IPlayer | null;
+}
