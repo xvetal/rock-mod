3a8551bedc1e87ff6b7dff74a821c200ef874dab
xvetal <djratmir@gmail.com>
2024-09-23 05:29:27 +0300

    Added methods for vectors.

--- src/server/common/utils/math/Vectors.ts
+++ src/server/common/utils/math/Vectors.ts
@@ -15,6 +15,22 @@ export class Vector2D {
     this._x = x;
     this._y = y;
   }
+
+  public distanceTo(position: Vector2D): number {
+    return Math.sqrt(this.squaredDistanceTo(position));
+  }
+
+  public squaredDistanceTo(position: Vector2D): number {
+    const { x, y } = position;
+
+    return (this.x - x) * (this.x - x) + (this.y - y) * (this.y - y);
+  }
+
+  public isInRange(position: Vector2D, range: number): boolean {
+    const squaredDistance = this.squaredDistanceTo(position);
+
+    return squaredDistance <= range * range;
+  }
 }
 
 export class Vector3D extends Vector2D {
@@ -27,5 +43,21 @@ export class Vector3D extends Vector2D {
   public constructor(x: number, y: number, z: number) {
     super(x, y);
     this._z = z;
+  }
+
+  public override distanceTo(position: Vector3D): number {
+    return Math.sqrt(this.squaredDistanceTo(position));
+  }
+
+  public override squaredDistanceTo(position: Vector3D): number {
+    const { x, y, z } = position;
+
+    return (this.x - x) * (this.x - x) + (this.y - y) * (this.y - y) + (this.z - z) * (this.z - z);
+  }
+
+  public override isInRange(position: Vector3D, range: number): boolean {
+    const squaredDistance = this.squaredDistanceTo(position);
+
+    return squaredDistance <= range * range;
   }
 }
