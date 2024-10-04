c41900f2f6d8db87a82877881dbc5f3f8c363605
xvetal <djratmir@gmail.com>
2024-10-05 02:14:46 +0300

    Added subscription to many events.

--- src/server/net/common/events/IEventsManager.ts
+++ src/server/net/common/events/IEventsManager.ts
@@ -3,17 +3,16 @@ import { IBaseObject } from "../../../entities/common/baseObject/IBaseObject";
 
 export interface INetServerEvents {
   "rm::playerConnected"(player: IPlayer): void;
-  "rm:playerDisconnected"(player: IPlayer): void;
+  "rm::playerDisconnected"(player: IPlayer): void;
   "rm::entityCreated"(entity: IBaseObject): void;
   "rm::entityDestroyed"(entity: IBaseObject): void;
 }
 
 export interface IEventsManager {
-  on<K extends keyof INetServerEvents>(
-    eventName: K,
-    listener: (...args: Parameters<INetServerEvents[K]>) => void,
+  on<K extends keyof INetServerEvents>(events: Record<K, (...args: Parameters<INetServerEvents[K]>) => void>): void;
+  onServer<K extends keyof INetServerEvents>(
+    events: Record<K, (...args: Parameters<INetServerEvents[K]>) => void>,
   ): void;
-  onServer(eventName: string, listener: (...args: unknown[]) => void): void;
   off(eventName: string, listener: (...args: unknown[]) => void): void;
   emit(eventName: string, ...args: unknown[]): void;
   emitClient(player: IPlayer, eventName: string, ...args: unknown[]): void;
