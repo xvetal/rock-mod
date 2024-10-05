ebdabb789e4419b5a82db6ef29929717d09074cb
xvetal <djratmir@gmail.com>
2024-10-05 04:06:20 +0300

    Added rotation field to entity.

--- src/server/entities/altv/entity/AltVEntity.ts
+++ src/server/entities/altv/entity/AltVEntity.ts
@@ -2,6 +2,8 @@ import { IEntity } from "../../common/entity/IEntity";
 import { AltVWorldObject, IAltVWorldObjectOptions } from "../worldObject/AltVWorldObject";
 import hash = AltVShared.hash;
 import Entity = AltVServer.Entity;
+import { Vector3D } from "../../../common/utils";
+import Vector3 = AltVShared.Vector3;
 
 export interface IAltVEntityOptions<T extends Entity> extends IAltVWorldObjectOptions<T> {}
 
@@ -10,11 +12,20 @@ export abstract class AltVEntity<T extends Entity> extends AltVWorldObject<T> im
     return this.mpEntity.model;
   }
 
+  public get rotation(): Vector3D {
+    const { x, y, z } = this.mpEntity.rot;
+    return new Vector3D(x, y, z);
+  }
+
   protected constructor(options: IAltVEntityOptions<T>) {
     super(options);
   }
 
   public setModel(value: string): void {
     this.mpEntity.model = hash(value);
+  }
+
+  public setRotation(value: Vector3D): void {
+    this.mpEntity.rot = new Vector3(value);
   }
 }
--- src/server/entities/common/entity/IEntity.ts
+++ src/server/entities/common/entity/IEntity.ts
@@ -1,8 +1,11 @@
 import { IWorldObject, IWorldObjectOptions } from "../worldObject/IWorldObject";
+import { IVector3D } from "../../../common/utils";
 
 export interface IEntityOptions extends IWorldObjectOptions {}
 
 export interface IEntity extends IWorldObject {
   get model(): number;
+  get rotation(): IVector3D;
   setModel(value: string): void;
+  setRotation(value: IVector3D): void;
 }
