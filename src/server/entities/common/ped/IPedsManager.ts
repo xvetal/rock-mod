cd9296772191d6a4114c2223206e18c5ded22737
xvetal <djratmir@gmail.com>
2024-11-17 18:42:00 +0300

    Added frozen option for peds in ragemp.

--- src/server/entities/common/ped/IPedsManager.ts
+++ src/server/entities/common/ped/IPedsManager.ts
@@ -1,7 +1,9 @@
 import { IEntitiesManager, IEntityCreateOptions } from "../entity";
 import { IPed } from "./IPed";
 
-export interface IPedCreateOptions extends IEntityCreateOptions {}
+export interface IPedCreateOptions extends IEntityCreateOptions {
+  frozen: boolean;
+}
 
 export interface IPedsManager extends IEntitiesManager<IPed> {
   create(options: IPedCreateOptions): IPed;
