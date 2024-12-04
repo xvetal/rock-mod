dd5bf81a3a1173fe0037d051c57864b4b42c3adf
Carter <djratmir@gmail.com>
2024-12-05 01:36:34 +0300

    refactor(server): improve exports organization in index.ts
    - Replace wildcard exports with named exports for better control
    - Add explicit IRockMod prefix to entity interfaces
    - Add network events and RPC interface exports

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,16 @@
 export * from "./RockMod";
-export * from "./entities";
 export * from "./common/utils";
+export {
+  IBaseObject as IRockModBaseObject,
+  IBlip as IRockModBlip,
+  IColshape as IRockModColshape,
+  IEntity as IRockModEntity,
+  IMarker as IRockModMarker,
+  IObject as IRockModObject,
+  IPed as IRockModPed,
+  IPlayer as IRockModPlayer,
+  IVehicle as IRockModVehicle,
+  IWorldObject as IRockModWorldObject,
+} from "./entities";
+export { INetServerEvents, INetClientEvents } from "./net/common/events/IEventsManager";
+export { INetServerRPC, INetClientRPC } from "./net/common/rpc/IRPCManager";
