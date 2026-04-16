28d64d72f660d0d5966f81611f34fd6796a28ef2
Volodymyr Anufriiev <v.anufriev2003@gmail.com>
2026-04-16 03:02:50 +0300

    feat(client-implementation): extended IObject.ts entity

--- src/client/entities/common/object/IObject.ts
+++ src/client/entities/common/object/IObject.ts
@@ -2,4 +2,22 @@ import { type IEntity, type IEntityOptions } from "../entity";
 
 export interface IObjectOptions extends IEntityOptions {}
 
-export interface IObject extends IEntity {}
+export interface IObject extends IEntity {
+  attachTo(
+    entity: Handle,
+    boneIndex: number,
+    xPos: number,
+    yPos: number,
+    zPos: number,
+    xRot: number,
+    yRot: number,
+    zRot: number,
+    useSoftPinning: boolean,
+    collision: boolean,
+    isPed: boolean,
+    vertexIndex: number,
+    fixedRot: boolean,
+  ): void;
+  isAttachedTo(entity: number): boolean;
+  detach(applyVelocy: boolean, collision: boolean): void;
+}
--- src/client/entities/ragemp/object/RageObject.ts
+++ src/client/entities/ragemp/object/RageObject.ts
@@ -7,4 +7,45 @@ export class RageObject extends RageEntity<ObjectMp> implements IObject {
   public constructor(options: IRageObjectOptions) {
     super(options);
   }
+
+  public attachTo(
+    entity: Handle,
+    boneIndex: number,
+    xPos: number,
+    yPos: number,
+    zPos: number,
+    xRot: number,
+    yRot: number,
+    zRot: number,
+    useSoftPinning: boolean,
+    collision: boolean,
+    isPed: boolean,
+    vertexIndex: number,
+    fixedRot: boolean,
+  ): void {
+    this.mpEntity.attachTo(
+      entity,
+      boneIndex,
+      xPos,
+      yPos,
+      zPos,
+      xRot,
+      yRot,
+      zRot,
+      false,
+      useSoftPinning,
+      collision,
+      isPed,
+      vertexIndex,
+      fixedRot,
+    );
+  }
+
+  public isAttachedTo(entity: number): boolean {
+    return this.mpEntity.isAttachedTo(entity);
+  }
+
+  public detach(applyVelocy: boolean, collision: boolean): void {
+    this.mpEntity.detach(applyVelocy, collision);
+  }
 }
