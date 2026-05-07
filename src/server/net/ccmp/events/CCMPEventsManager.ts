848918cc65f129a91e9879926e275c7af7900f8d
xvetal <xvetal@proton.me>
2026-05-07 23:14:54 +0300

    feat(ccmp): implement event handling methods in CCMPEventsManager

--- src/server/net/ccmp/events/CCMPEventsManager.ts
+++ src/server/net/ccmp/events/CCMPEventsManager.ts
@@ -1,48 +1,59 @@
+import { type CcmpServerBuiltinEvents } from "@classic-mp/types/server";
 import { type IEventsManager } from "../../common/events/IEventsManager";
 import { type IServerInternalEvents } from "../../common/events/types";
 import { type IClientToServerEvents, type IServerToClientEvents } from "../../../../shared";
 import { type CCMPPlayer } from "../../../entities/ccmp/player/CCMPPlayer";
 
-const notImplemented = (name: string): never => {
-  throw new Error(`Not implemented yet: ${name}`);
-};
+export interface ICCMPServerInternalEvents extends IServerInternalEvents, CcmpServerBuiltinEvents {}
 
 export class CCMPEventsManager implements IEventsManager {
-  public onInternal(_events: Partial<IServerInternalEvents>): void {
-    notImplemented("CCMPEventsManager.onInternal");
+  public onInternal(events: Partial<ICCMPServerInternalEvents>): void {
+    for (const eventName of Object.keys(events)) {
+      // @ts-expect-error mixed CCMP built-in + rm:: keys typing
+      ccmp.on(eventName, events[eventName]);
+    }
   }
 
-  public offInternal<K extends keyof IServerInternalEvents>(_eventName: K, _listener: IServerInternalEvents[K]): void {
-    notImplemented("CCMPEventsManager.offInternal");
+  public offInternal<K extends keyof IServerInternalEvents>(eventName: K, listener: IServerInternalEvents[K]): void {
+    ccmp.off(eventName, listener as (...args: unknown[]) => void);
   }
 
   public emitInternal<K extends keyof IServerInternalEvents>(
-    _eventName: K,
-    ..._args: Parameters<IServerInternalEvents[K]>
+    eventName: K,
+    ...args: Parameters<IServerInternalEvents[K]>
   ): void {
-    notImplemented("CCMPEventsManager.emitInternal");
+    ccmp.emit(eventName, ...args);
   }
 
-  public onClient(_events: Partial<IClientToServerEvents>): void {
-    notImplemented("CCMPEventsManager.onClient");
+  public onClient(events: Partial<IClientToServerEvents>): void {
+    for (const eventName of Object.keys(events)) {
+      // @ts-expect-error generic event typing
+      ccmp.on(eventName, events[eventName]);
+    }
   }
 
-  public offClient<K extends keyof IClientToServerEvents>(_eventName: K, _listener: IClientToServerEvents[K]): void {
-    notImplemented("CCMPEventsManager.offClient");
+  public offClient<K extends keyof IClientToServerEvents>(eventName: K, listener: IClientToServerEvents[K]): void {
+    ccmp.off(eventName, listener as (...args: unknown[]) => void);
   }
 
   public emitClient<K extends keyof IServerToClientEvents>(
-    _player: CCMPPlayer,
-    _eventName: K,
-    ..._args: Parameters<IServerToClientEvents[K]>
+    player: CCMPPlayer,
+    eventName: K,
+    ...args: Parameters<IServerToClientEvents[K]>
   ): void {
-    notImplemented("CCMPEventsManager.emitClient");
+    const ccmpPlayer = ccmp.players.getById(player.id);
+
+    if (!ccmpPlayer) {
+      throw new Error(`Player with id ${player.id} not found`);
+    }
+
+    ccmpPlayer.emit(eventName, args);
   }
 
   public emitAllClients<K extends keyof IServerToClientEvents>(
-    _eventName: K,
-    ..._args: Parameters<IServerToClientEvents[K]>
+    eventName: K,
+    ...args: Parameters<IServerToClientEvents[K]>
   ): void {
-    notImplemented("CCMPEventsManager.emitAllClients");
+    ccmp.emitAllClients(eventName, args);
   }
 }
