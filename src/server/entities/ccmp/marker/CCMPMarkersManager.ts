fa686acc9afe41f3cc4c244d1adcdc8a03391450
xvetal <xvetal@proton.me>
2026-05-11 01:49:22 +0300

    feat(ccmp): implement CCMPMarkersManager via ccmp.markers API

--- src/server/entities/ccmp/marker/CCMPMarkersManager.ts
+++ src/server/entities/ccmp/marker/CCMPMarkersManager.ts
@@ -1,10 +1,6 @@
 import { type IMarkerCreateOptions, type IMarkersManager } from "../../common/marker/IMarkersManager";
 import { CCMPWorldObjectsManager } from "../worldObject/CCMPWorldObjectsManager";
-import { type CCMPMarker } from "./CCMPMarker";
-
-const notImplemented = (name: string): never => {
-  throw new Error(`Not implemented yet: ${name}`);
-};
+import { CCMPMarker } from "./CCMPMarker";
 
 export interface ICCMPMarkerCreateOptions extends IMarkerCreateOptions {}
 
@@ -15,7 +11,24 @@ export class CCMPMarkersManager extends CCMPWorldObjectsManager<CCMPMarker> impl
     });
   }
 
-  public create(_options: ICCMPMarkerCreateOptions): CCMPMarker {
-    return notImplemented("CCMPMarkersManager.create");
+  public create(options: ICCMPMarkerCreateOptions): CCMPMarker {
+    const { position, dimension, type, scale, color, rotation } = options;
+
+    const ccmpMarker = ccmp.markers.create(type, position.x, position.y, position.z, scale, {
+      color: { r: color.r, g: color.g, b: color.b, a: color.a ?? 255 },
+      rotation: { x: rotation.x, y: rotation.y, z: rotation.z },
+      dimension,
+    });
+    if (!ccmpMarker) {
+      throw new Error("CCMPMarkersManager.create: ccmp.markers.create failed (server full?)");
+    }
+
+    const marker = new CCMPMarker({
+      ccmpMarker,
+      onDestroy: (m): void => this.unregisterBaseObject(m),
+    });
+    this.registerBaseObject(marker);
+
+    return marker;
   }
 }
