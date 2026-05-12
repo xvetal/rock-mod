9a383863f9c3a6dc71102f1abbca72bcf61276f2
xvetal <xvetal@proton.me>
2026-05-12 03:36:24 +0300

    feat(ccmp): implement CCMPPed setters via ccmp.peds API

--- src/server/entities/ccmp/ped/CCMPPed.ts
+++ src/server/entities/ccmp/ped/CCMPPed.ts
@@ -2,6 +2,8 @@ import { CCMPEntity } from "../entity/CCMPEntity";
 import { type IPed } from "../../common/ped/IPed";
 import { BaseObjectType } from "../../../../shared";
 import { type IVector3D, Vector3D } from "../../../../shared/common/utils/math/Vectors";
+import { MathClamp } from "../../../../shared/common/utils/math/Math";
+import { RockMod } from "../../../RockMod";
 import type { Ped as CcmpPed } from "@classic-mp/types/server";
 
 export interface ICCMPPedOptions {
@@ -69,20 +71,34 @@ export class CCMPPed extends CCMPEntity implements IPed {
     this._onDestroy(this);
   }
 
-  public override setPosition(_value: IVector3D): void {
-    throw new Error("CCMPPed.setPosition: not supported by CCMP yet");
+  public override setPosition(value: IVector3D): void {
+    this._ccmpPed.teleport(value.x, value.y, value.z);
   }
 
   public override setDimension(_value: number): void {
     throw new Error("CCMPPed.setDimension: not supported by CCMP yet");
   }
 
-  public override setModel(_value: string): void {
-    throw new Error("CCMPPed.setModel: not supported by CCMP yet");
+  public override setModel(value: string): void {
+    // CCMP ped.model is u32; hash the name via the shared util before assignment.
+    this._ccmpPed.model = RockMod.instance.utils.hash(value);
   }
 
-  public override setRotation(_value: IVector3D): void {
-    throw new Error("CCMPPed.setRotation: not supported by CCMP yet");
+  public override setRotation(value: IVector3D): void {
+    // CCMP exposes only heading; pitch/roll (x/y) are silently dropped.
+    this._ccmpPed.heading = value.z;
+  }
+
+  public setHeading(value: number): void {
+    this._ccmpPed.heading = value;
+  }
+
+  public setHealth(value: number): void {
+    this._ccmpPed.health = MathClamp(value, 0, 200);
+  }
+
+  public setArmour(value: number): void {
+    this._ccmpPed.armour = MathClamp(value, 0, 100);
   }
 
   public override getNetData(_name: string): unknown {
