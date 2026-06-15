22bfd2704998fa024f4636ea63b2510c12461429
xvetal <xvetal@proton.me>
2026-06-15 15:27:28 +0300

    feat(server): add colshape net data support

--- src/server/entities/common/colshape/IColshape.ts
+++ src/server/entities/common/colshape/IColshape.ts
@@ -2,4 +2,7 @@ import { type IWorldObject, type IWorldObjectOptions } from "../worldObject";
 
 export interface IColshapeOptions extends IWorldObjectOptions {}
 
-export interface IColshape extends IWorldObject {}
+export interface IColshape extends IWorldObject {
+  getNetData(name: string): unknown;
+  setNetData(name: string, value: unknown): void;
+}
